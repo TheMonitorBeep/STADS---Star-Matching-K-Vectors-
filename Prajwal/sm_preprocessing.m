8c506a7813372b10da8e153bfcbc2f43fa22702b
Jamun-Fanatic-Foreva <prajwal.1305@gmail.com>
2020-04-11 16:22:47 +0530

    UPDATE Reference Catalogue
    UPDATE  sm_preprocessing.m
    Scipt now uses sm_gnrt_K_VEC.m function.
    
    UPDATE sm_true_values.m
    Script can now run independently. Fixed variable nomenclature.
    
    UPDATE Reference_Catalogue.csv
    Append K-Vector generated using sm_gnrt_K_VEC.m

--- Prajwal/sm_preprocessing.m
+++ Prajwal/sm_preprocessing.m
@@ -3,33 +3,12 @@ addpath(genpath('.\Prajwal'));
 
 %% Read Catalogues
 GD_CAT = readmatrix('.\Prajwal\Catalogues\Guide_Catalogue.csv'); % Read - Guide catalogue
-PP_CAT = readmatrix('.\Prajwal\Catalogues\PreProcessed_Catalogue.csv'); % Read - Reference catalogue (which contains the star pairs)
+PP_CAT = readmatrix('.\Prajwal\Catalogues\PreProcessed_Catalogue.csv'); % Read - Preprocessed catalogue (which contains the star pairs)
 
-c_ANG_DST = PP_CAT(:,4); % Extract 'Angular distance' from Reference catalogue
-
-%% Initialize variables
-u = 2.22*1.0e-16; % Precision of the machine 
-
-sz = size(PP_CAT); % Size of REF_CAT
-n_rw_RC = sz(1); % Number of star pairs (Number of rows - Reference catalogue)
-
-c_z = zeros(n_rw_RC, 1);
-K_VEC = zeros(n_rw_RC, 1);
-
-%% Calculate constants
-m = ( max(c_ANG_DST) - min(c_ANG_DST) + 2*u ) / ( n_rw_RC - 1 );
-q = min(c_ANG_DST) - u - m ;
+c_ANG_DST = PP_CAT(:,4); % Extract 'Angular distance' from Preprocessed catalogue
 
 %% Construct K-Vector
-for i_rw = 1:n_rw_RC             
-    c_z(i_rw) = m*i_rw + q;
-    K_VEC(i_rw) = i_rw - 1;
-    
-    if c_z(i_rw) > c_ANG_DST(i_rw)
-        K_VEC(i_rw) = K_VEC(i_rw) + 1;
-    end
-    
-end
+[K_VEC,~,~] = sm_gnrt_K_VEC( c_ANG_DST, 1 );
 
 %% Create Reference Catalogue
 REF_CAT = [PP_CAT, K_VEC]; % Append K-Vector column to Reference catalogue 
